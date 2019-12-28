a647564774fd6518ecf73bda87b6461c83227813
AlexC <55947391+AlexCards@users.noreply.github.com>
2019-12-28 22:38:25 +0300

    28.12.2019

--- js/fullCatalog.js
+++ js/fullCatalog.js
@@ -1,5 +1,15 @@
 const fullCatalog = document.querySelector(".fullCatalog");
+let fullCatalogBtn = getComputedStyle(
+  document.querySelector("#fullCatalogBtn"),
+  ":before"
+).getPropertyValue("content");
+
+console.log(fullCatalogBtn);
+
 
 function showFullContainer() {
   fullCatalog.classList.toggle("fullCatalogOnClick");
+  fullCatalogBtn = `url("http://127.0.0.1:5500/SVG_Icons/close.svg")`
+  console.log(fullCatalogBtn);
+  
 }
