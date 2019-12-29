bb90aceec69e6b440370d7c3c14eff69f9ecef7a
AlexC <55947391+AlexCards@users.noreply.github.com>
2019-12-29 23:48:53 +0300

    29.12.2019

--- js/fullCatalog.js
+++ js/fullCatalog.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const fullCatalog = document.querySelector(".fullCatalog");
+  const btn = document.querySelector("#fullContentBtn");
 
-  showFullContainer = () => {
-    fullCatalog.classList.toggle("fullCatalogOnClick");
-  };
+  fullCatalog.style.height = "0px";
+
+  btn.addEventListener("click", () => {
+    if(fullCatalog.style.height === "0px"){ //open
+      fullCatalog.style.height = "310px";
+      fullCatalog.style.transition = "height .5s ease"
+    } else{ //close 
+      fullCatalog.style.height = "0px";
+    }
+  })
 });
