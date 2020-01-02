ebb6d805e51836ad1981286e15982585d7aa87eb
AlexC <55947391+AlexCards@users.noreply.github.com>
2020-01-02 21:55:59 +0300

    02.01.2020

--- js/fullCatalog.js
+++ js/fullCatalog.js
@@ -1,19 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   const fullCatalog = document.querySelector(".fullCatalog");
-  const btn = document.querySelector("#fullContentBtn");
+  const btn = document.querySelector(".fullContentBtn");
 
   fullCatalog.style.height = "0px";
+  document.querySelector(".fullCatalog table").style.display = "none";
 
-  btn.addEventListener("click", () => {
+  const menuIco = new Image();
+  menuIco.src = "/SVG_Icons/menu.svg";
+
+  const appender = btn.appendChild(menuIco);
+
+  btn.addEventListener("click", function() {
     if (fullCatalog.style.height === "0px") {
       //open
+      menuIco.src = "/SVG_Icons/close.svg";
+
       fullCatalog.style.height = "310px";
       fullCatalog.style.transition = "height .5s ease";
+
+      this.classList.add("close");
+
+      document.querySelector(".fullCatalog table").style.display = "block";
     } else {
       //close
+      document.querySelector(".fullCatalog table").style.display = "none";
+      
+      menuIco.src = "/SVG_Icons/menu.svg";
+
       fullCatalog.style.height = "0px";
+
+      this.classList.remove("close");
     }
   });
-
-  
 });
