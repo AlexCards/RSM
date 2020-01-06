a505f7561e6acde5f72ccda9c75a73f92c7291ff
AlexC <55947391+AlexCards@users.noreply.github.com>
2020-01-06 20:49:45 +0300

    shoping cart 0.0

--- js/fullCatalog.js
+++ js/fullCatalog.js
@@ -10,24 +10,49 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const appender = btn.appendChild(menuIco);
 
+  const openFullCatalog = () => {
+    document.querySelector(
+      ".container"
+    ).style.gridTemplateRows = `70px 70px auto 340px`;
+
+    document.querySelector(
+      ".container"
+    ).style.gridTemplateAreas = `"header header" "nav nav" "fullCatalog fullCatalog" "slider catalog"`;
+  };
+
+  const closeFullCatalog = () => {
+    document.querySelector(
+      ".container"
+    ).style.gridTemplateRows = `70px 70px 340px`;
+
+    document.querySelector(
+      ".container"
+    ).style.gridTemplateAreas = `"header header" "nav nav" "slider catalog"`;
+  };
+
   btn.addEventListener("click", function() {
     if (fullCatalog.style.height === "0px") {
       //open
       menuIco.src = "/SVG_Icons/close.svg";
 
+      openFullCatalog();
+
       fullCatalog.style.height = "310px";
-      fullCatalog.style.transition = "height .3s ease";
+      fullCatalog.style.transition = "height .2s ease";
 
       this.classList.add("close");
 
-      document.querySelector(".fullCatalog table").style.display = "inline-block";
+      document.querySelector(".fullCatalog table").style.display =
+        "inline-block";
     } else {
       //close
       document.querySelector(".fullCatalog table").style.display = "none";
-      
+
       menuIco.src = "/SVG_Icons/menu.svg";
 
       fullCatalog.style.height = "0px";
+
+      setTimeout(closeFullCatalog, 200);
 
       this.classList.remove("close");
     }
